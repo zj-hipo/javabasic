96203a3235cc8d609713ceae2e56647b71ab0c19
zj-hipo <542699293@qq.com>
2020-08-05 10:29:23 +0800

    测试分支

---
+++ src/main/java/com/zj/javabasic/javaTest/News.java
@@ -0,0 +1,12 @@
+package com.zj.javabasic.javaTest;
+
+/**
+ * @program: javabasic
+ * @description: 填写描述
+ * @author: 张军
+ * @create: 2020-08-05 10:28
+ **/
+
+public class News {
+    private String Name;
+}
